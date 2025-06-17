3d0c44e9441d6d13bd25af6c9951acbd8fa596f7
Nazir <nazirazizis123456@gmail.com>
2025-06-17 20:31:49 +0430

    Finished StudentDashboardView

--- Educational_Center_Management_System/ViewModels/StudentDashboardViewModel.cs
+++ Educational_Center_Management_System/ViewModels/StudentDashboardViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Educational_Center_Management_System.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace Educational_Center_Management_System.ViewModels
     class StudentDashboardViewModel
     {
         public Student Student { get; set; }
-
+        public ObservableCollection<ScoreForDisplay> Scores { get; set; }
         public StudentDashboardViewModel(/*Student student*/)
         {
             Student = new Student
@@ -26,6 +27,25 @@ namespace Educational_Center_Management_System.ViewModels
                 State = 1,
                 Password = "faty"
 
+            };
+            Scores = new ObservableCollection<ScoreForDisplay>
+            {
+                new ScoreForDisplay
+                {
+                    ClassId = 101,
+                    ClassName = "Prep",
+                    Number = 91.9M,
+                    TeacherName = "Azizi",
+                    ClassDate = new DateOnly(2024, 1, 6)
+                },
+                new ScoreForDisplay
+                {
+                    ClassId = 1,
+                    ClassName = "Advance A",
+                    Number = 56.7M,
+                    TeacherName = "Azizi",
+                    ClassDate = new DateOnly(2024, 5, 6)
+                }
             };
         }
     }
