0750cb1522fe5974fba6e28212e4cf0977b72de2
Nazir <nazirazizis123456@gmail.com>
2025-06-18 18:33:06 +0430

    Finished StudentDashboard and connected it to StudentServiceRepository to get the data from Database

--- Educational_Center_Management_System/ViewModels/LogInViewModel.cs
+++ Educational_Center_Management_System/ViewModels/LogInViewModel.cs
@@ -2,6 +2,7 @@
 using Educational_Center_Management_System.Services.Interfaces;
 using System.Windows.Input;
 using Educational_Center_Management_System.Helpers;
+using Educational_Center_Management_System.Models;
 
 namespace Educational_Center_Management_System.ViewModels
 {
@@ -31,8 +32,21 @@ namespace Educational_Center_Management_System.ViewModels
         }
         private void ExecuteLogIn(object? parameter)
         {
+            Student student = new Student
+            {
+                Id = 1,
+                Name = "Nazir",
+                Fathername = "Mohammad Aziz",
+                LastName = "Azizi",
+                BirthDate = new DateOnly(2003, 1, 19),
+                Photo = null,
+                PhoneNumber = "0783360601",
+                JoinDate = new DateOnly(2024, 4, 12),
+                State = 1,
+                Password = "Hooooo"
+            };
             // when button clicked
-            _navigationService.NavigateTo(new StudentDashboardViewModel());
+            _navigationService.NavigateTo(new StudentDashboardViewModel(student));
         }
     }
 }
--- Educational_Center_Management_System/ViewModels/StudentDashboardViewModel.cs
+++ Educational_Center_Management_System/ViewModels/StudentDashboardViewModel.cs
@@ -1,135 +1,36 @@
-﻿using Educational_Center_Management_System.Models;
+﻿using Educational_Center_Management_System.DataAccessLayer;
+using Educational_Center_Management_System.Models;
+using Educational_Center_Management_System.Services;
 using System.Collections.ObjectModel;
 
 namespace Educational_Center_Management_System.ViewModels
 {
     class StudentDashboardViewModel
     {
+        StudentService _studentService;
         public Student Student { get; set; }
         public ObservableCollection<ScoreForDisplay> Scores { get; set; }
         public ObservableCollection<Class> Classes { get; set; }
-        public StudentDashboardViewModel(/*Student student*/)
+        public StudentDashboardViewModel(Student student)
         {
-            Student = new Student
-            {
-                Id = 1,
-                Name = "Nazir",
-                LastName = "Azizi",
-                Fathername = "Mohammad Aziz",
-                BirthDate = new DateOnly(2005, 12, 19),
-                Photo = null, // Assuming photo is not required for this test
-                PhoneNumber = "0783360601",
-                JoinDate = new DateOnly(2030, 9, 1),
-                State = 1,
-                Password = "faty"
-
-            };
-            Scores = new ObservableCollection<ScoreForDisplay>
-            {
-                new ScoreForDisplay
-                {
-                    ClassId = 101,
-                    ClassName = "Prep",
-                    Number = 91.9M,
-                    TeacherName = "Azizi",
-                    ClassDate = new DateOnly(2024, 1, 6)
-                },
-                new ScoreForDisplay
-                {
-                    ClassId = 1,
-                    ClassName = "Advance A",
-                    Number = 56.7M,
-                    TeacherName = "Azizi",
-                    ClassDate = new DateOnly(2024, 5, 6)
-                },
-                new ScoreForDisplay
-                {
-                    ClassId = 101,
-                    ClassName = "Prep",
-                    Number = 91.9M,
-                    TeacherName = "Azizi",
-                    ClassDate = new DateOnly(2024, 1, 6)
-                },
-                new ScoreForDisplay
-                {
-                    ClassId = 1,
-                    ClassName = "Advance A",
-                    Number = 56.7M,
-                    TeacherName = "Azizi",
-                    ClassDate = new DateOnly(2024, 5, 6)
-                },
-                new ScoreForDisplay
-                {
-                    ClassId = 101,
-                    ClassName = "Prep",
-                    Number = 91.9M,
-                    TeacherName = "Azizi",
-                    ClassDate = new DateOnly(2024, 1, 6)
-                },
-                new ScoreForDisplay
-                {
-                    ClassId = 1,
-                    ClassName = "Advance A",
-                    Number = 56.7M,
-                    TeacherName = "Azizi",
-                    ClassDate = new DateOnly(2024, 5, 6)
-                },
-                new ScoreForDisplay
-                {
-                    ClassId = 101,
-                    ClassName = "Prep",
-                    Number = 91.9M,
-                    TeacherName = "Azizi",
-                    ClassDate = new DateOnly(2024, 1, 6)
-                },
-                new ScoreForDisplay
-                {
-                    ClassId = 1,
-                    ClassName = "Advance A",
-                    Number = 56.7M,
-                    TeacherName = "Azizi",
-                    ClassDate = new DateOnly(2024, 5, 6)
-                },
-                new ScoreForDisplay
-                {
-                    ClassId = 101,
-                    ClassName = "Prep",
-                    Number = 91.9M,
-                    TeacherName = "Azizi",
-                    ClassDate = new DateOnly(2024, 1, 6)
-                },
-                new ScoreForDisplay
-                {
-                    ClassId = 1,
-                    ClassName = "Advance A",
-                    Number = 56.7M,
-                    TeacherName = "Azizi",
-                    ClassDate = new DateOnly(2024, 5, 6)
-                }
-            };
-            Classes = new ObservableCollection<Class>
-            {
-                new Class
-                {
-                    Id = 1023,
-                    State = 1,
-                    Teacher = 1,
-                    Semester = "AP",
-                    Name = "Prep",
-                    Time = new TimeSpan(6, 30,0),
-                    Date = new DateOnly(2024, 5, 1)
-                },
-                new Class
-                {
-                    Id = 5643,
-                    State = 1,
-                    Teacher = 2,
-                    Semester = "AZ",
-                    Name = "Level Six",
-                    Time = new TimeSpan(14, 0,0),
-                    Date = new DateOnly(2024, 10, 1)
-                }
-            };
+            Student = student;
+            _studentService = new StudentService();
+            Scores = new ObservableCollection<ScoreForDisplay>();
+            Classes = new ObservableCollection<Class>();
+            getListOfScores();
+            getListOfClasses();
+        }
+        private async Task getListOfScores()
+        {
+            List<ScoreForDisplay> listOfScores = await _studentService.GetScore(Student.Id);
+            foreach(ScoreForDisplay score in listOfScores)
+                Scores.Add(score);
+        }
+        private async Task getListOfClasses()
+        {
+            List<Class> listOfClasses = await _studentService.GetClass(Student.Id);
+            foreach (Class myClass in listOfClasses)
+                Classes.Add(myClass);
         }
     }
 }
